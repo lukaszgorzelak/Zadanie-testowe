33ee78e7cf0c0fc9e41d570434be57a2a63c2e45
Łukasz <lukaszgorzelak91@gmail.com>
2021-04-22 20:38:29 +0200

    deploy project

---
+++ js/main.js
@@ -0,0 +1,59 @@
+
+const innerWidth = window.innerWidth;
+
+   new Splide( '.splide', {
+       pagination  : false,
+       arrows:true,
+       perPage    : 1,
+       perMove    : 1,
+       height     : '5rem',
+       width      : '100%',
+       fixedHeight: '4rem',
+       focus      : 'left',
+       trimSpace  : true,
+       autoWidth: true,
+       autoHeight: true,
+       breakpoints: {
+        1200: {
+          pagination  : true,
+          arrows:true,
+          perPage    : 1,
+          perMove    : 1,
+          height     : '5rem',
+          width      : '100%',
+          fixedHeight: '4rem',
+          focus      : 'left',
+          trimSpace  : true,
+          autoWidth: true,
+          autoHeight: true,
+        },
+        370:{
+          perPage: 2,
+          focus: 'center',
+          trimSpace  : false,
+        }
+      },
+   } ).mount();
+  
+const close = document.querySelector('#modal__close');
+const play = document.querySelector('#play');
+const modal = document.querySelector('#modal');
+let mask = document.createElement("div");
+
+// mask.outerHTML = `<div class="banner"></div>`;
+const body = document.body;
+
+// $('body').append(`<div class="banner"></div>`);
+
+play.addEventListener('click', function(){
+  modal.style.display = "flex";
+  mask.classList.add("mask");
+  body.appendChild(mask);
+  console.log(mask)
+})
+
+close.addEventListener('click', function(){
+  modal.style.display = "none";
+  mask.classList.remove('mask');
+})
+  
